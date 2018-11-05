487b0db0a2fd4751b7e1d053894ecce9468541d4
lpernez <lpernez@gmail.com>
2018-11-05 16:07:05 +0100

    Création de l'IHM de base:4 boutons et 2 input text pour 2 moteurs

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+     <head>
+          <tilte>Mon premier site web</title>
+
+
+     </head>
+
+     <body>
+         <div id="titre1" class="titre" style="border:solid"> <h1>Interface de commande </h1></div>
+         <div>
+			 <h2>Moteur1</h2>
+	      <input type="button" value="+"/> <input type="text"> <input type="button"/><input type="button"/>
+         </div>
+         <div>
+          <h2>Moteur2</h2>
+	      <input type="button"/> <input type="text"> <input type="button"/><input type="button"/>
+         
+         </div> 
+             
+      </body>
+      </html>
